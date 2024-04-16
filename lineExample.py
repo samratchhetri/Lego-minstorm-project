0f7e6e8835ce88a683983a6ee98ab8eae7d4e9d7
omabo20 <72064792+omabo20@users.noreply.github.com>
2024-04-16 10:44:15 +0200

    Add files via upload

---
+++ lineExample.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env pybricks-micropython
+
+from pybricks.ev3devices import Motor, ColorSensor
+from pybricks.parameters import Port
+from pybricks.tools import wait
+from pybricks.robotics import DriveBase
+
+# Initialize the motors.
+left_motor = Motor(Port.A)
+right_motor = Motor(Port.B)
+
+# Initialize the color sensor.
+line_sensor = ColorSensor(Port.S1)
+
+# Initialize the drive base.
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
+# Calculate the light threshold. Choose values based on your measurements.
+BLACK = 9
+WHITE = 85
+threshold = (BLACK + WHITE) / 2
+
+# Set the drive speed at 100 millimeters per second.
+# Can you ramp this up?
+DRIVE_SPEED = 100
+
+# Start following the line endlessly.
+while True:
+    
+    # Use the turn rate as input to the system
+
+    # Set the drive base speed and turn rate.
+    robot.drive(DRIVE_SPEED, turn_rate)
+
+    # You can wait for a short time or do other things in this loop.
+    wait(10)
